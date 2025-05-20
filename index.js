ab20b0427a9cdd53a3c0e07dcc649acdb8ec02f7
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-05-20 16:19:23 +0600

    initial project

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-var express = require('express');
-var app = express();
-var port = 5022;
-app.get('/', function (req, res) {
-    res.send('Hello World!');
-});
-app.listen(port, function () {
-    console.log("Example app listening on port ".concat(port));
-});
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Server is running!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
